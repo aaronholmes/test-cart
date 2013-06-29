0ec1d4ae54fbe73ea32e30f72534f5a57665a3e8
Aaron Holmes <aholmes@Aarons-MacBook-Pro.local>
2013-06-29 15:27:43 -0400

    Initial rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestCart::Application.config.secret_key_base = 'a4e0b5aab9a2ef23498d39831b1f02beb967a6cc50d80b0ec14ed9eb4c9356ddb62bce894fc3ca828c76b0a02986b1207f6f31c0ebf8309054a3163ad4633307'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestCart::Application.config.session_store :cookie_store, key: '_test-cart_session'
